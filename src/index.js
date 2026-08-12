b7344615f666b132f8909e66348ca10816ce5b20
Basant_Jamwal <basantrajput570@gmail.com>
2026-08-12 22:08:39 +0530

    minor bugs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
 });
 
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log("app is listening on port : ", process.env.PORT)
+        });
+    })
+    .catch((error) => {
+        console.log("MongoDB error : ", error);
+    })
